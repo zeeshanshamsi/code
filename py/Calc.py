f74ce04a77b196799d6ba7e310f274f48abe8016
Zeeshan Shamsi <Zeeshan@Zeeshans-MacBook-Pro.local>
2019-07-19 14:07:26 -0700

    Calculator App

---
+++ py/Calc.py
@@ -0,0 +1,12 @@
+action = input('What do you want to do?')
+
+userInput1 = input('Please enter in a number 1')
+userInput2 = input('Please enter in a number 2')
+
+
+
+if action == 1:
+    print(userInput1 + userInput2)
+
+
+
