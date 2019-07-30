e0dff37a9626aaf8498659264d0212501dbb1739
Zeeshan Shamsi <Zeeshan@Zeeshans-MacBook-Pro.local>
2019-07-30 13:59:29 -0700

    created hangman and continued implementation on login

--- py/login.py
+++ py/login.py
@@ -1,3 +1,22 @@
+from os import system
+ 
+    
+class User: 
+
+    def __init__(self, username, password): 
+        self.username = username 
+        self.password = password
+        self.lockCount = 0
+
+    
+box = []
+
+zeeshan = User('Zeeshan', '4321')
+sam = User('Sam', '1234')
+
+box.append(zeeshan)
+box.append(sam)
+
 
 import threading
 def lockoutTimer():
@@ -7,34 +26,45 @@ def lockoutTimer():
 def Unlock():
     print('\nAccount is now unlocked')
 
-import getpass 
-storedPassword = "Hello"
+import getpass
 inputCheck = 1
-    
-counter=0
+
+
 while inputCheck == 1:
-    userInput1 = input('\nPlease Enter Username: ')
-    counter+=1 
+    userInput1 = input('\nUsername: ')
+    
+
     try: 
 	    p = getpass.getpass(prompt = "Password: ")
     except Exception as error: 
 	    print('ERROR', error)   
     
-    if p == storedPassword:
-        print('\nWelcome!\n')
-        inputCheck = 0
-    else:
-#        print('Incorrect Password - this is attempt: ' + str(counter))
-        print('\nIncorrect Password - Attempt Number: {}'.format(counter))
-        
-        if counter == 3:
-            print('\nLocked Out')
-            inputCheck = 0
-            lockoutTimer()
+
+    for x in box:
+#        print('{0} is the username, {1} is the password'.format(x.username, x.password))
+        if x.username == userInput1:
+
+            if x.password == p:
+                system('clear')
+                print('\nWelcome, {}'.format(x.username))
+                inputCheck = 0
+
+            else: 
+                
+                x.lockCount+=1
+                system('clear')
+                if x.lockCount == 3:
+                    print('\nLocked Out')
+                    x.lockCount = 0
+                    inputCheck = 0
+                    lockoutTimer()
+                print('\nPassword Incorrect. Please Try Again. Attempts Remaining: {}'.format((3 - x.lockCount)))
+
+
 
             
-        
-       
-   
-    
-            
+
+#           print('Username Not Found: {}'.format(userInput1))
+
+#        print('Incorrect Password - this is attempt: ' + str(counter))
+#            print('\nIncorrect Password - Attempt Number: {}'.format(counter))
