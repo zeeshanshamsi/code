ee75e3f747e2af5e4ac1ec9c8a4925e0acaf00dc
Zeeshan Shamsi <Zeeshan@Zeeshans-MacBook-Pro.local>
2019-08-02 13:51:43 -0700

    created launch file and imported hangman and calc to login file

--- py/Calc.py
+++ py/Calc.py
@@ -1,44 +1,45 @@
-print('Hello! Welcome to the calculator! Please select \
-what you would like to do')
-action = input('Add = 1 Subtract = 2 Multiply = 3 Divide = 4: ')
+def calcRun():
+    print('Hello! Welcome to the calculator! Please select \
+    what you would like to do')
+    action = input('Add = 1 Subtract = 2 Multiply = 3 Divide = 4: ')
 
-if action == '1' or action == '2' or action == '3' or action == '4':
-    inputCheck = 1
+    if action == '1' or action == '2' or action == '3' or action == '4':
+        inputCheck = 1
 
 
-    while inputCheck == 1:
-        userInput1 = input('Please enter in a number 1: ')
-        
-        if userInput1.isnumeric():
-            userInput1 = int(userInput1)
-            inputCheck = 0
+        while inputCheck == 1:
+            userInput1 = input('Please enter in a number 1: ')
+            
+            if userInput1.isnumeric():
+                userInput1 = int(userInput1)
+                inputCheck = 0
+            else:
+                print('Enter a real number!')
+
+
+        inputCheck = 1
+        while inputCheck == 1:
+            userInput2 = input('Please enter in a number 2: ')
+            
+            if userInput2.isnumeric():
+                userInput2 = int(userInput2)
+                inputCheck = 0
+            else:
+                print('Enter a real number!')
+
+
+        if action == '1':
+            print(userInput1 + userInput2)
+
+        elif action == '2':
+            print(userInput1 - userInput2)
+
+        elif action == '3':
+            print(userInput1 * userInput2)
+
+        elif action == '4':
+            print(userInput1 // userInput2)
         else:
-            print('Enter a real number!')
-
-
-    inputCheck = 1
-    while inputCheck == 1:
-        userInput2 = input('Please enter in a number 2: ')
-        
-        if userInput2.isnumeric():
-            userInput2 = int(userInput2)
-            inputCheck = 0
-        else:
-            print('Enter a real number!')
-
-
-    if action == '1':
-        print(userInput1 + userInput2)
-
-    elif action == '2':
-        print(userInput1 - userInput2)
-
-    elif action == '3':
-        print(userInput1 * userInput2)
-
-    elif action == '4':
-        print(userInput1 // userInput2)
+            print('Everything was false!')
     else:
-        print('Everything was false!')
-else:
-    print('Input Wrong')
+        print('Input Wrong')
--- py/login.py
+++ py/login.py
@@ -1,83 +1,142 @@
-from os import system
-counter = 0
-    
-class User: 
+def loginRun():
+    from Calc import calcRun
 
-    def __init__(self, username, password): 
-        self.username = username 
-        self.password = password
-        self.lockCount = 0
+    from Hangman import HangRun
 
-    
-box = []
+    from os import system
+    counter = 0
+        
 
-zeeshan = User('Zeeshan', '4321')
-sam = User('Sam', '1234')
+    class User: 
 
-box.append(zeeshan)
-box.append(sam)
+        def __init__(self, username, password, accountType): 
+            self.username = username 
+            self.password = password
+            self.lockCount = 0
+            self.accountType = accountType 
 
-import threading
-def lockoutTimer():
-    timer = threading.Timer(3.0, Unlock)
-    timer.start() 
+        
+    box = []
 
-def Unlock():
-    print('\nAccount is now unlocked')
+    zeeshan = User('Zeeshan', '1234', 'admin')
+    sam = User('Sam', '1234', 'standard')
+    bob = User('Bob', '1234', 'standard')
+    drake = User('Drake', '1234', 'standard')
 
 
 
-def GlobalLock(a):
-    a+=1
-    print(a)
-    return a
+    box.append(zeeshan)
+    box.append(sam)
+    box.append(bob)
+    box.append(drake)
+
+    import threading
+    def lockoutTimer():
+        timer = threading.Timer(3.0, Unlock)
+        timer.start() 
+
+    def Unlock():
+        print('\nAccount is now unlocked')
 
 
 
+    def GlobalLock(a):
+        a+=1
+        print(a)
+        return a
 
-import getpass
-inputCheck = 1
+    def UserAccountTypeCheck():
+        if x.accountType == 'admin': 
+            ChooseApp()
+        else: 
+            HangRun()
 
 
-while inputCheck == 1:
-    userInput1 = input('\nUsername: ')
-    
+    def ChooseApp():
+        inputCheck = 0
+        while inputCheck == 0:
+            print('\nChoose App: 1 = Calculator and 2 = Hangman\n ')
+            AppInput1 = input('\nChoice #: ')
 
-    try: 
-	    p = getpass.getpass(prompt = "Password: ")
-    except Exception as error: 
-	    print('ERROR', error)   
-    
-
-    for x in box:
-
-        if x.username == userInput1:
-
-            if x.password == p:
-                system('clear')
-                print('\nWelcome, {}\n'.format(x.username))
-                inputCheck = 0
-                break
+            if AppInput1 == '1': 
+                inputCheck = 1
+                calcRun()
+                
+                
+            
+            elif AppInput1 == '2':
+                inputCheck = 1
+                HangRun()
 
             else: 
-                
-                x.lockCount+=1
                 system('clear')
-                
-                
-                print('\nPassword Incorrect for User: {0}. Attempts Remaining: {1}'.format(x.username, (3 - x.lockCount)))
+                print('Input Incorrect')
+            
 
-                if x.lockCount == 3:
-                    print('\nLocked Out')
-                    x.lockCount = 0
-                    #lockoutTimer()
+
+
+
+
+
+
+
+    import getpass
+    inputCheck = 1
+
+
+    while inputCheck == 1:
+        userInput1 = input('\nUsername: ')
+        
+
+        try: 
+            p = getpass.getpass(prompt = "Password: ")
+        except Exception as error: 
+            print('ERROR', error)   
+        
+
+
+
+
     
+        for x in box:
+            if x.username == userInput1:
 
-    counter+=1
-    if counter == 10:
-        print('\nExceeded Attempt Limit - Exiting Program\n')
-        break
+                if x.password == p:
+                    system('clear')
+                    print('\nWelcome, {}\n'.format(x.username))
+                    #inputCheck = 0
+                    UserAccountTypeCheck()
+                    break
 
-    if userInput1 == 'exit':
-        break 
-    
+                else: 
+                    
+                    x.lockCount+=1
+                    system('clear')
+                    
+                    
+                    print('\nPassword Incorrect for User: {0}. Attempts Remaining: {1}'.format(x.username, (3 - x.lockCount)))
+
+
+
+
+
+                    if x.lockCount == 3:
+                        print('\nLocked Out')
+                        x.lockCount = 0
+                        #lockoutTimer()
+            
+
+    #            indexCheck = box.index(x)
+    #            if len(box) == indexCheck + 1:
+    #                clearIt = 1
+
+
+
+        counter+=1
+        if counter == 10:
+            print('\nExceeded Attempt Limit - Exiting Program\n')
+            break
+
+        if userInput1 == 'exit':
+            break 
+        
