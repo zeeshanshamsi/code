28f67067e7d743dddf21e9b3d3d9b1f642c609fe
Zeeshan Shamsi <Zeeshan@Zeeshans-MacBook-Pro.local>
2019-08-05 13:58:27 -0700

    added while loop to make sure program does not exit

--- py/Calc.py
+++ py/Calc.py
@@ -1,45 +1,82 @@
 def calcRun():
-    print('Hello! Welcome to the calculator! Please select \
-    what you would like to do')
-    action = input('Add = 1 Subtract = 2 Multiply = 3 Divide = 4: ')
-
-    if action == '1' or action == '2' or action == '3' or action == '4':
-        inputCheck = 1
 
 
-        while inputCheck == 1:
-            userInput1 = input('Please enter in a number 1: ')
-            
-            if userInput1.isnumeric():
-                userInput1 = int(userInput1)
-                inputCheck = 0
+
+    while True: 
+
+
+
+
+
+
+
+
+
+
+        print('\nWelcome to the calculator! Please select what you would like to do: \n')
+        action = input('Add = 1 Subtract = 2 Multiply = 3 Divide = 4: ')
+
+        if action == '1' or action == '2' or action == '3' or action == '4':
+            inputCheck = 1
+
+
+            while inputCheck == 1:
+                userInput1 = input('\nPlease enter in a number 1: ')
+                
+                if userInput1.isnumeric():
+                    userInput1 = int(userInput1)
+                    inputCheck = 0
+                else:
+                    print('Enter a real number!')
+
+
+            inputCheck = 1
+            while inputCheck == 1:
+                userInput2 = input('Please enter in a number 2: ')
+                
+                if userInput2.isnumeric():
+                    userInput2 = int(userInput2)
+                    inputCheck = 0
+                else:
+                    print('Enter a real number!')
+
+
+            if action == '1':
+                print('\nAnswer = ' + str((userInput1 + userInput2)))
+
+            elif action == '2':
+                print('\nAnswer = ' + str((userInput1 - userInput2)))
+
+            elif action == '3':
+                print('\nAnswer = ' + str((userInput1 * userInput2)))
+
+            elif action == '4':
+                print('\nAnswer = ' + str((userInput1 // userInput2)))
             else:
-                print('Enter a real number!')
-
-
-        inputCheck = 1
-        while inputCheck == 1:
-            userInput2 = input('Please enter in a number 2: ')
-            
-            if userInput2.isnumeric():
-                userInput2 = int(userInput2)
-                inputCheck = 0
-            else:
-                print('Enter a real number!')
-
-
-        if action == '1':
-            print(userInput1 + userInput2)
-
-        elif action == '2':
-            print(userInput1 - userInput2)
-
-        elif action == '3':
-            print(userInput1 * userInput2)
-
-        elif action == '4':
-            print(userInput1 // userInput2)
+                print('Everything was false!')
         else:
-            print('Everything was false!')
-    else:
-        print('Input Wrong')
+            print('Input Wrong')
+
+        
+        
+        
+        
+        
+        
+        launchInput = input('Continue = 1 Exit = 2: ')
+
+        if launchInput == '1' or launchInput == '2':
+
+            if launchInput == '1':
+                print('Hello')
+            else:
+                print('\nExiting Program')
+                break 
+
+
+
+
+
+
+
+    
